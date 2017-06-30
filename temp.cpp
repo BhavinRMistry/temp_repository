568507335d3a94b7fc6290d3ebcb0b865b0b940e
Bhavin Mistry <bhavin.mistry.r@gmail.com>
2017-06-30 13:45:03 -0400

    Initial commit

---
+++ temp.cpp
@@ -0,0 +1,16 @@
+//June 30, 2017
+//Temp CPP file to upload it on Github
+#include <iostream>
+
+using namespace std;
+
+int int main()
+{
+  int n;
+
+  cout << "Enter any integer number to display: ";
+  cin >> n;
+  cout << " Entered value = " << n << endl;
+
+  return 0;
+}
